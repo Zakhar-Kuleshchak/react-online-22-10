691a8b70cb2f371d296b985357b201022f78ec69
Zakhar <zahkul94@gmail.com>
2023-01-24 19:35:46 +0200

    add list <>

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// without jsx
-// const title = React.createElement('h1', { id: 'title' }, 'Hello React.js')
-// console.log(title)
+function List() {
+    return (
+        <>
+            <ul>
+                <li>list</li>
+                <li>list</li>
+                <li>list</li>
+            </ul>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nisi
+                quisquam expedita molestiae quod sit nemo laboriosam! Ut rerum,
+                nobis distinctio quaerat reiciendis inventore aspernatur
+                repellat, ullam saepe vitae doloremque.
+            </p>
+        </>
+    )
+}
 
-// jsx => js extended
-// tsx => ts extended
-const title = <h1 id="title">Hello React.js</h1>
-console.log(title)
+function Title() {
+    return <h1>Hello TS</h1>
+}
 
-const content = (
-    <div>
-        {title}
-        {10 + 10}
-        <ul className="list">
-            <li>List item 1</li>
-            <li>List item 2</li>
-            <li>List item 3</li>
-        </ul>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
-            magni minima, sequi nostrum quia velit rem laudantium. Ipsam laborum
-            minima consequatur, nobis, odio eius dolor esse, nisi doloribus
-            excepturi aperiam!
-        </p>
-    </div>
-)
+const App = () => {
+    return (
+        <div className="app">
+            <Title />
+            <List />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
