def89e4d9643d293e8a73a484c819077427e5731
Zakhar <zahkul94@gmail.com>
2023-01-28 10:18:01 +0200

    додав дефолтне значення для пропса нейм і воно працює

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { List } from './List'
 
 type TitleProps = {
-    name: string
+    name?: string
     text: string
     year: number
 }
 
-const Title = ({ name, text, year }: TitleProps) => {
+const Title = ({ name = 'Jack', text, year }: TitleProps) => {
     return (
         <>
             <h1>Hello {name}</h1>
@@ -22,7 +22,6 @@ const App = () => {
     return (
         <div className="app">
             <Title
-                name="App"
                 text="Lorem ipsum dolor sit amet consectetur adipisicing elit."
                 year={2023}
             />
