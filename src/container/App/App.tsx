cec3e2870e435cda42aeb3cd63288e9c20536fca
Zakhar <zahkul94@gmail.com>
2023-01-29 11:10:08 +0200

    add header, css green background

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,40 +1,19 @@
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import { Container } from '@mui/material'
+import Footer from 'container/Footer/Footer'
+import Header from 'container/Header/Header'
+import Main from 'container/Main/Main'
+import CssBaseline from '@mui/material/CssBaseline'
+import { StyledEngineProvider } from '@mui/material/styles'
 
 type Props = {}
 
 const App = (props: Props) => {
     return (
-        <>
-            <AppBar position="static">
-                <Container>
-                    <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography
-                            variant="h6"
-                            component="div"
-                            sx={{ flexGrow: 1 }}
-                        >
-                            News
-                        </Typography>
-                        <Button color="inherit">Login</Button>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-        </>
+        <StyledEngineProvider injectFirst>
+            <CssBaseline />
+            <Header />
+            <Main />
+            <Footer />
+        </StyledEngineProvider>
     )
 }
 
