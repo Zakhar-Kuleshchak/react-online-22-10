48958663a376f909993f7392c82fb49d3399924d
Zakhar <zahkul94@gmail.com>
2023-02-04 11:52:30 +0200

    add props for productsListItem

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -11,13 +11,31 @@ const ProductsList = (props: Props) => {
             </Typography>
             <Grid container spacing={4}>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 14 Pro"
+                        description="This is iPhone 14 Pro"
+                        capacity="256"
+                        type="phone"
+                        price={1500}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 13 Pro"
+                        description="This is iPhone 13 Pro"
+                        capacity="128"
+                        type="phone"
+                        price={1000}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title="iPhone 12 Pro"
+                        description="This is iPhone 12 Pro"
+                        capacity="512"
+                        type="phone"
+                        price={500}
+                    />
                 </Grid>
             </Grid>
         </>
