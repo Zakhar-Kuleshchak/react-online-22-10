d414aad86dfbe147e53d42bc8ab9adf3e9f99f92
Zakhar <zahkul94@gmail.com>
2023-02-13 14:19:15 +0200

    add count 2 in counter

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -17,29 +17,36 @@ type Props = {
     image: string
 }
 
-class ProductsListItem extends Component<Props> {
+type State = {
+    count: number
+}
+
+class ProductsListItem extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            count: 2,
+        }
+    }
+
     render() {
         const { title, description, capacity, type, price, image } = this.props
         return (
             <Card className="product" variant="outlined">
                 <CardContent>
                     <div className="product-image">
-                        <img src={this.props.image} alt="" />
+                        <img src={image} alt="" />
                     </div>
-                    <div className="product-title">{this.props.title}</div>
-                    <div className="product-description">
-                        {this.props.description}
-                    </div>
+                    <div className="product-title">{title}</div>
+                    <div className="product-description">{description}</div>
+                    <div className="product-features">Type: {type}</div>
                     <div className="product-features">
-                        Type: {this.props.type}
+                        Capacity: {capacity} Gb
                     </div>
-                    <div className="product-features">
-                        Capacity: {this.props.capacity} Gb
-                    </div>
-                    <div className="product-price">{this.props.price}$</div>
+                    <div className="product-price">{price}$</div>
                     <div className="product-quantity">
                         <Button variant="outlined">-</Button>
-                        <TextField size="small" value={1} />
+                        <TextField size="small" value={this.state.count} />
                         <Button variant="outlined">+</Button>
                     </div>
                 </CardContent>
